b57da290cafe713c1902e14e2c22704be37c534b
chengmo23 <53300044+chengmo23@users.noreply.github.com>
2023-08-04 12:22:31 +0800

    add project sub

---
+++ src/main/java/com/chengmo/controller/ProjectController.java
@@ -0,0 +1,12 @@
+package com.chengmo.controller;
+
+/**
+ * Create by chengmo at 2023/08/04
+ */
+@Log4j2
+@Controller
+@RequestMapping("/project")
+public class ProjectController {
+
+    public 
+}
---
+++ src/main/java/com/chengmo/service/ProjectService.java
@@ -0,0 +1,25 @@
+package com.chengmo.service;
+
+import com.chengmo.app.Project;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+/**
+ * Create by chengmo at 2023/08/04
+ */
+
+public interface ProjectService {
+
+    Project findByProjectName(String projectName);
+
+    List<Project> findAll();
+
+    boolean addProject(Project project);
+
+    boolean updateProject(Project project);
+
+    boolean deleteProjectById(Integer[] ids);
+
+
+}
